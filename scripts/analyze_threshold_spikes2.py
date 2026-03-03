98e0667b36a956b18ead9eba734daa30e269591d
iwb5@duke.edu <iwb5@duke.edu>
2026-03-03 07:06:12 -0600

    update analyze spikes2

--- scripts/analyze_threshold_spikes2.py
+++ scripts/analyze_threshold_spikes2.py
@@ -7,8 +7,10 @@ import seaborn as sns
 import pandas as pd
 
 import spikeinterface.full as si
-from spikeinterface.sortingcomponents.peak_detection import detect_peaks
-from spikeinterface.sortingcomponents.peak_localization import localize_peaks
+from spikeinterface.preprocessing import compute_motion, load_motion_info
+# from spikeinterface.sortingcomponents.peak_detection import detect_peaks
+# from spikeinterface.sortingcomponents.motion import estimate_motion
+# from spikeinterface.sortingcomponents.peak_localization import localize_peaks
 import spikeglx
 from powerpixels import Pipeline
 
@@ -33,6 +35,8 @@ def get_ni_pulse_times(rec_path):
         if len(onsets) > 0 and len(offsets) > 0:
             onsets = onsets[onsets < offsets[-1]]
             offsets = offsets[offsets > onsets[0]]
+        else:
+            continue
         ch_pulse = {
             'onset': onsets,
             'offset': offsets
@@ -42,23 +46,7 @@ def get_ni_pulse_times(rec_path):
     return pulse_times
 
 
-def analyze_time_chunk(rec_slice):
-    job_kwargs = dict(n_jobs=40, chunk_duration='1s', progress_bar=True)
-    peaks = detect_peaks(
-        rec_slice,  method='locally_exclusive',
-        method_kwargs=dict(
-            detect_threshold=5,
-            radius_um=50.
-        ),
-        job_kwargs=job_kwargs
-    )
-    peak_locations = localize_peaks(
-        rec_slice, peaks, method='center_of_mass', job_kwargs=job_kwargs
-    )
-    return peaks, peak_locations
-
-
-def main(rec_path: Path):
+def main(rec_path: Path, psth_range=(-1.0, 1.0)):
     pp = Pipeline(
         PPP_path / 'config' / 'threshold_settings.json',
         rec_path
@@ -93,68 +81,104 @@ def main(rec_path: Path):
         # Load in raw data
         rec = pp.load_raw_binary()
 
-        if not (pp.results_path / 'threshold_peaks.npy').exists():
+        if not (pp.results_path / 'threshold_motion').exists():
             # Preprocessing
             rec = pp.preprocessing(rec)
 
             job_kwargs = dict(n_jobs=40, chunk_duration='1s', progress_bar=True)
-            peaks = detect_peaks(
-                rec,  method='locally_exclusive',
-                method_kwargs=dict(
+            # peaks = detect_peaks(
+            #     rec,  method='locally_exclusive',
+            #     method_kwargs=dict(
+            #         detect_threshold=5,
+            #         radius_um=50.
+            #     ),
+            #     job_kwargs=job_kwargs
+            # )
+            # peak_locations = localize_peaks(
+            #     rec, peaks, method='center_of_mass', job_kwargs=job_kwargs
+            # )
+            motion, motion_info = compute_motion(
+                recording=rec,
+                preset='dredge_fast',
+                folder=pp.results_path / 'threshold_motion',
+                output_motion_info=True,
+                detect_kwargs=dict(
+                    method='locally_exclusive',
                     detect_threshold=5,
-                    radius_um=50.
+                    radius_um=50.,
                 ),
-                job_kwargs=job_kwargs
+                localize_peaks_kwargs=dict(method='center_of_mass'),
+                **job_kwargs
             )
-            peak_locations = localize_peaks(
-                rec, peaks, method='center_of_mass', job_kwargs=job_kwargs
-            )
-            np.save(pp.results_path / 'threshold_peaks.npy', peaks)
-            np.save(pp.results_path / 'threshold_locations.npy', peak_locations)
+            # np.save(pp.results_path / 'threshold_peaks.npy', peaks)
+            # np.save(pp.results_path / 'threshold_locations.npy', peak_locations)
         else:
-            peaks = np.load(pp.results_path / 'threshold_peaks.npy')
-            peak_locations = np.load(pp.results_path / 'threshold_locations.npy')
+            motion_info = load_motion_info(pp.results_path / 'threshold_motion')
 
         fs = rec.sampling_frequency
-        times = peaks['sample_index'] / fs
+        fig = plt.figure(figsize=(14, 8))
+        si.plot_motion_info(
+            motion_info, rec,
+            figure=fig,
+            color_amplitude=True,
+            amplitude_cmap="inferno",
+            scatter_decimate=10,
+        )
+        fig.savefig(pp.results_path / 'threshold_motion.png')
+        times = motion_info['peaks']['sample_index'] / fs
+        channels = motion_info['peaks']['channel_index']
+        labels = np.zeros(times.shape, dtype=int)
+        locs_x = motion_info['peak_locations']['x']
+        locs_y = motion_info['peak_locations']['y']
 
-        fig, ax = plt.subplots(1, 1, sharex=False, sharey=False)
-        ax.scatter(peaks['sample_index'] / fs, peak_locations['y'], color='k', marker='.', alpha=0.002)
+        sec_bins = np.arange(min(times), max(times), 1.0)
+        millisec_bins = np.arange(min(times), max(times), 0.1)
+        millisec_labels = np.zeros(millisec_bins.shape, dtype=int)
 
+        fig, ax = plt.subplots()
+        ax.hist(times, bins=sec_bins)
         ch_peaks = {}
         for i, (chan, onoff) in enumerate(pulse_times.items()):
-            ch_peaks[chan] = []
             for j, (on, off) in enumerate(zip(onoff['onset'], onoff['offset'])):
                 if j == 0:
                     label = chan
                 else:
                     label = None
                 ax.axvspan(on, off, color=colors[i], alpha=0.3, label=label)
-                ch_peaks[chan].append(peaks[np.logical_and(peaks['sample_index'] / fs > on, peaks['sample_index'] / fs < off)])
-            if len(ch_peaks[chan]) == 0:
-                del ch_peaks[chan]
+                labels[np.logical_and(times > on, times < off)] = i+1
+                millisec_labels[np.logical_and(millisec_bins > on, millisec_bins < off)] = i+1
         ax.legend()
 
-        fig, ax = plt.subplots(1, 1)
-        ax.hist(peaks['sample_index']/fs, bins=round(np.max(peaks['sample_index']) / fs // 1.0))
+        fig, axs = plt.subplots(len(pulse_times), 1)
+        psth_dict = {}
         for i, (chan, onoff) in enumerate(pulse_times.items()):
-            for j, (on, off) in enumerate(zip(onoff['onset'], onoff['offset'])):
-                if j == 0:
-                    label = chan
-                else:
-                    label = None
-                ax.axvspan(on, off, color=colors[i], alpha=0.3, label=label)
-        ax.legend()
-
-        fig, axs = plt.subplots(len(ch_peaks), 1)
-        for i, (chan, spike_list) in enumerate(ch_peaks.items()):
-            chan_spikes = []
-            for spikes, onset in zip(spike_list, pulse_times[chan]['onset']):
-                chan_spikes.append(spikes['sample_index'] / fs - onset)
-            chan_spikes = np.hstack(chan_spikes)
-            axs[i].hist(chan_spikes, bins=np.arange(min(chan_spikes), max(chan_spikes), 0.1))
-            axs[i].set_title(chan)
+            baseline_times = []
+            psth_times = []
+            for onset in onoff['onset']:
+                baseline_times.append(times[np.logical_and(times > onset + psth_range[0], times < onset)] - onset)
+                psth_times.append(times[np.logical_and(times >= onset, times < onset + psth_range[1])] - onset)
+            baseline_times = np.hstack(baseline_times)
+            psth_times = np.hstack(psth_times)
+            axs[i].hist(baseline_times, bins=np.arange(psth_range[0], 0.1, 0.1), color='gray')
+            axs[i].hist(psth_times, bins=np.arange(0, psth_range[1]+0.1, 0.1), color=colors[i])
         plt.tight_layout()
+        fig.savefig(pp.results_path / 'threshold_psth')
+
+        u_chan = np.unique(motion_info['peaks']['channel_index'])
+        fig, axs = plt.subplots(1, len(pulse_times), sharex=True, sharey=True)
+        for i, chan in enumerate(pulse_times.keys()):
+            responder_inds = np.zeros(times.shape, dtype=bool)
+            for ch in u_chan:
+                bl_times = times[np.logical_and(channels == ch, labels==0)]
+                ch_times = times[np.logical_and(channels == ch, labels==i+1)]
+                bl_fr = len(bl_times) / (np.sum(millisec_labels == 0) * 0.1)
+                ch_fr = len(ch_times) / (np.sum(millisec_labels == i+1) * 0.1)
+                if ch_fr > bl_fr:
+                    responder_inds = np.logical_or(responder_inds, np.logical_and(channels == ch, labels==i+1))
+            si.plot_probe_map(rec, ax=axs[i], with_channel_ids=False)
+            axs[i].scatter(locs_x[responder_inds], locs_y[responder_inds], color='red', alpha=0.01)
+        fig.savefig(pp.results_path / 'threshold_map')
+
 
         plt.show()
 
